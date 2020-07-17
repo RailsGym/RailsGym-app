93703513d32423df236bae58b76809903e3067a3
Matsushin <matsu1983@gmail.com>
2020-07-17 23:39:26 +0900

    パラメータを渡すように

--- railsgym/pages/categories/_id/items/index.vue
+++ railsgym/pages/categories/_id/items/index.vue
@@ -16,9 +16,22 @@ export default {
       category: {}
     }
   },
-  apollo: {
-    category: {
-      query: category
+  async created () {
+    await this.fetchCategory(this.$route.params.id)
+  },
+  methods: {
+    async fetchCategory (categoryId) {
+      try {
+        const res = await this.$apollo.query({
+          query: category,
+          variables: {
+            id: parseInt(categoryId, 10)
+          }
+        })
+        this.category = res.data.category
+      } catch (e) {
+        console.log(e)
+      }
     }
   }
 }
