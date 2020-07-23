ce6cdc98de3fc8e7a976dbc9baac01bdd7e530bb
Matsushin <matsu1983@gmail.com>
2020-07-23 16:13:52 +0900

    ログインの見た目調整

--- railsgym/pages/login.vue
+++ railsgym/pages/login.vue
@@ -1,52 +1,57 @@
 <template>
   <v-container>
-    <v-row>
-      <v-col cols="6" offset="3">
-        <v-card>
-          <v-card-title>ログイン</v-card-title>
-          <v-card-text>
-            <v-form>
-              <v-container>
-                <v-row>
-                  <v-col cols="6">
-                    <v-text-field v-model="email" label="メールアドレス" required />
-                  </v-col>
-                </v-row>
-                <v-row>
-                  <v-col cols="6">
-                    <v-text-field v-model="password" type="password" label="パスワード" required />
-                  </v-col>
-                </v-row>
-                <v-row>
-                  <v-col>
-                    <v-btn
-                      color="orange"
-                      dark
-                      @click="login()">
-                      ログイン
-                    </v-btn>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-form>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
+    <h2 class="page-title">
+      ログイン
+    </h2>
+    <v-form>
+      <v-container class="mt-10">
+        <v-row>
+          <v-col cols="8">
+            <Errors :errors="errors" />
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="8">
+            <v-text-field v-model="email" label="メールアドレス" required />
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="8">
+            <v-text-field v-model="password" type="password" label="パスワード" required />
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-btn
+              color="orange"
+              dark
+              @click="login()">
+              ログイン
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-form>
   </v-container>
 </template>
 
 <script>
+import Errors from '@/components/shared/Errors'
+
 export default {
   middleware ({ store, redirect }) {
     if (store.state.auth.loggedIn) {
       return redirect('/')
     }
   },
+  components: {
+    Errors
+  },
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      errors: []
     }
   },
   methods: {
@@ -61,8 +66,10 @@ export default {
           }
         })
         await this.$apolloHelpers.onLogin(this.$auth.getToken('local').match(/^Bearer[ ]+([^ ]+)[ ]*$/i)[1])
+        this.$toast.info('ログインしました')
         this.$router.push('/')
       } catch (e) {
+        this.errors = ['メールアドレスまたはパスワードが正しくありません。']
         console.log(e)
       }
     }
