34808dd70760d473117265d066c517fe11783a39
ryo-funaba <ryo278.0417@gmail.com>
2020-08-01 16:02:40 +0900

    レビュー表示修正

--- railsgym/pages/categories/_category/items/_id.vue
+++ railsgym/pages/categories/_category/items/_id.vue
@@ -17,17 +17,30 @@
       {{ ymdhms(item.createdAt) }}
     </p>
     <hr>
-    <v-card v-for="review in reviews" :key="review.id">
-      <v-btn
-        color="success"
-        :to="{ name: 'categories-category-items-review-edit', params: { category: $route.params.category, id: review.id }}">編集</v-btn>
-      <v-btn
-        color="red"
-        dark>
-        削除</v-btn>
-      <p>{{ review.user.username }}</p>
-      <p v-html="nl2br(review.content)" />
-      <p>{{ ymdhms(review.createdAt) }}</p>
+    <h3 class="mt-4 mb-4">レビュー一覧</h3>
+    <v-card v-for="review in reviews" :key="review.id" class="mb-4">
+      <v-card-text>
+        <div class="d-flex pb-4">
+          <div>
+            {{ review.user.username }}
+          </div>
+          <div class="ml-auto">
+            {{ ymdhms(review.createdAt) }}
+          </div>
+        </div>
+        <p v-html="nl2br(review.content)"/>
+      </v-card-text>
+      <div class="d-flex pb-4">
+        <div class="ml-auto">
+          <v-btn
+            color="success"
+            :to="{ name: 'categories-category-items-review-edit', params: { category: $route.params.category, id: review.id }}">編集</v-btn>
+          <v-btn
+            color="red"
+            dark>
+            削除</v-btn>
+        </div>
+      </div>
     </v-card>
     <div v-if="$auth.loggedIn" class="d-flex flex-row-reverse mt-6 mr-6">
       <v-btn
